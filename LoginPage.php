6ac311ada8dfb1557fe11518b12edf64ccfb3f78
SandeshOnRails <30823990+SandeshOnRails@users.noreply.github.com>
2018-03-01 21:50:21 -0800

    Update LoginPage.php

--- LoginPage.php
+++ LoginPage.php
@@ -1,43 +1,146 @@
 <?php
-session_start();
-$id = session_id();
+	include("includes/config.php");
+	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
+
+	$account = new Account($con);
+
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="EggFarmCSS.css"/>
+	<title>Simple Banking System</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
 </head>
+<body>
+	<?php
 
-<div class="panel">
-    <img id="sunPosition" src="../img/eggPlant2_files/image001.png" class="sunPosition">
-    <br>
-    <h1 style="text-align:center">Login</h1>
-    <br>
-    <form action="Status_page.php" method="post" name ="loginInput" onsubmit ="return(validateInput())">
-        Username<input type="text" name="Username" required>
-        <br>
-        Password<input type="password" name="Password" required>
-        <br>
-    <input type="submit" value="Submit">
-    </form>
-    <img src="../img/eggPlant2_files/ultrarare_blueegg.png" class="eggDecoration2">
+	if(isset($_POST['registerButton'])) {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+					$("#registerForm").show();
+				});
+			</script>';
+	}
+	else {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").show();
+					$("#registerForm").hide();
+				});
+			</script>';
+	}
 
-</div>
-<script>
-    function validateInput(){
-     if (document.loginInput.Username.value == ""){
-         alert ("Please enter Username")
-     }
-     if(document.loginInput.Password.value == ""){
-         alert ("Please enter Password")
-     }
-     else{
-         return true;
-     }
-    }
+	?>
+	
 
+	<div id="background">
+
+		<div id="loginContainer">
+
+			<div id="inputContainer">
+				<form id="loginForm" action="register.php" method="POST">
+					<h2>Login to your account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$loginFailed); ?>
+						<label for="loginUsername">Username</label>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('loginUsername') ?>" required>
+					</p>
+					<p>
+						<label for="loginPassword">Password</label>
+						<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+					</p>
+
+					<button type="submit" name="loginButton">LOG IN</button>
+
+					<div class="hasAccountText">
+						<span id="hideLogin">Don't have an account yet? Signup here.</span>
+					</div>
+					
+				</form>
+
+
+
+				<form id="registerForm" action="register.php" method="POST">
+					<h2>Create your free account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$usernameCharacters); ?>
+						<?php echo $account->getError(Constants::$usernameTaken); ?>
+						<label for="username">Username</label>
+						<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+						<label for="firstName">First name</label>
+						<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+						<label for="lastName">Last name</label>
+						<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+						<?php echo $account->getError(Constants::$emailInvalid); ?>
+						<?php echo $account->getError(Constants::$emailTaken); ?>
+						<label for="email">Email</label>
+						<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+					</p>
+
+					<p>
+						<label for="email2">Confirm email</label>
+						<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+						<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+						<?php echo $account->getError(Constants::$passwordCharacters); ?>
+						<label for="password">Password</label>
+						<input id="password" name="password" type="password" placeholder="Your password" required>
+					</p>
+
+					<p>
+						<label for="password2">Confirm password</label>
+						<input id="password2" name="password2" type="password" placeholder="Your password" required>
+					</p>
+
+					<button type="submit" name="registerButton">SIGN UP</button>
+
+					<div class="hasAccountText">
+						<span id="hideRegister">Already have an account? Log in here.</span>
+					</div>
+					
+				</form>
+
+
+			</div>
+
+			<div id="loginText">
+				
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
 
 
 
